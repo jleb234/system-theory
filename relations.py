5018f35409ed426f5bf12f1069ed45bda341bef6
Roman Milovanov <rmilovanov@gmail.com>
2022-04-25 10:12:54 +0400

    relations refactoring

--- relations.py
+++ relations.py
@@ -1,68 +1,63 @@
-from enum import Enum
 from nodes import *
 from rules import trigger_rules
 
-
-# class RelationType(Enum):
-#     REQUIRED = 'REQUIRED'
-#     OPTIONAL = 'OPTIONAL'
-#     GENERATED = 'GENERATED'
+# Relation types
+REQUIRED = 'REQUIRED'
+OPTIONAL = 'OPTIONAL'
+GENERATED = 'GENERATED'
 
 
 class RelationItem:
-    # rel_type = RelationType.REQUIRED
     rel_name = None
     constraints = []
 
     def __init__(self, source: NodeItem, target: NodeItem):
         self.source = source
         self.target = target
-        self.check_acceptable()
+        self.validate()
 
-    def check_acceptable(self):
-        """Хотя бы один из вариантов source-target из наборов ограничений должен быть валидным.
-        Поскольку все наследники имеют в labels метку предка, именно по этой метке определяется,
-        подходит этот класс для этого типа отношений или нет."""
+    @property
+    def relation_type(self):
+        for src, trg, rel_type in self.constraints:
+            if src in self.source.labels and trg in self.target.labels:
+                return rel_type
+        return None
 
-        results = [(src in self.source.labels) and (trg in self.target.labels) for src, trg, _ in self.constraints]
-        if not any(results):  # Хотя бы один должен быть валидным
+    def validate(self):
+        if self.relation_type is None:
             raise RuntimeError("You can not use {}-{} source-target combination in a relation of type {}".format(
                 type(self.source).__name__, type(self.target).__name__, type(self).__name__))
 
     def db_create_relation(self, connection):
         source_id = self.source.get_node_id(connection)
         target_id = self.target.get_node_id(connection)
-        rel_type = None
-        for src, trg, rel_type in self.constraints:
-            if src in self.source.labels and trg in self.target.labels:
-                rel_type = rel_type
         query = f"MATCH (source), (target) " \
                 f"WHERE ID(source) = {source_id} AND ID(target) = {target_id} " \
-                f"MERGE (source)-[:{rel_type} {{name: '{self.rel_name}'}}]->(target)"
+                f"CREATE (source)-[:{self.relation_type} {{name: '{self.rel_name}'}}]->(target)"
         connection.query(query)
         trigger_rules(connection)
 
 
 class Preceede(RelationItem):
     rel_name = "предшествовать"
-    constraints = [("ScenarioStep", "ScenarioStep", "OPTIONAL")]
+    constraints = [("ScenarioStep", "ScenarioStep", OPTIONAL)]
 
 
 class Include(RelationItem):
     rel_name = "включать в себя"
-    constraints = [("Scenario", "ScenarioStep", "REQUIRED"), ("Block", "Element", "REQUIRED")]
+    constraints = [("Scenario", "ScenarioStep", REQUIRED), ("Block", "Element", REQUIRED)]
 
 
 class BePartOf(RelationItem):
     rel_name = "быть частью"
-    constraints = [("Element", "Block", "OPTIONAL")]
+    constraints = [("Element", "Block", OPTIONAL)]
 
 
 class BePerformedOn(RelationItem):
     rel_name = "осуществляться на"
-    constraints = [("ScenarioStep", "Screen", "REQUIRED")]
+    constraints = [("ScenarioStep", "Screen", REQUIRED)]
 
 
 class InteractWith(RelationItem):
     rel_name = "предполагать взаимодействие с"
-    constraints = [("ScenarioStep", "Element", "OPTIONAL"), ("ScenarioStep", "Block", "OPTIONAL")]
+    constraints = [("ScenarioStep", "Element", OPTIONAL), ("ScenarioStep", "Block", OPTIONAL)]
